ced34664bd5eea2da5565c9134c3bc27e3bafb8e
k- <mahat.mejor@gmail.com>
2019-10-26 13:47:41 -0500

    No se subio
    Creo que rompi algo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-const formulario = document.querySelector("#carta-a-santa");
-function validarCiudad() {
-    console.log(formulario.ciudad);
+function validarNombre(nombre){
+    if(nombre.length===0) return "Poco";
+    if(nombre.length>=50) return "Mucho";
+    const patt1 = /[0-9]|\s|[!@#$%^&*()+-,.?":{}|<>]/g;
+    if(nombre.match(patt1).length>=1) return "Caracter Invalido";
+    return '';
 }
 
-function validarDescripcionRegalo() {
-    
+function validarCiudad(ciudad) {
+    if(!ciudad) return "No hay ciudad"
+    return ""
+}
+
+function validarDescripcionRegalo(descripcion) {
+    if(!descripcion) return "No hay descripcion"
+    if(descripcion.length>=250) return "Mucha descripcion"
+    return ""
 }
