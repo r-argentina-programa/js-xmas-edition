e6b7fbdf6e0a97e7159e982cb5531cd946d16044
ClassicalJo <55909151+ClassicalJo@users.noreply.github.com>
2019-10-27 12:54:28 +1100

    crea los archivos necesarios para la tarea de la clase 7
    modifica el index.html y pruebas para acomodar el codigo de los archivos nuevos.

---
+++ js/main-jose.js
@@ -0,0 +1,14 @@
+let $form = document.querySelector("[name=formulario]")
+
+function validarForm(event) {
+    event.preventDefault()
+    manejarErrores()
+    if (verificarTodo() === true) { exito() }
+}
+
+
+
+
+
+
+
