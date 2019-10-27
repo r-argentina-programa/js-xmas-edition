f66378190f655732ec637f5581c3a5338dd117b3
ClassicalJo <55909151+ClassicalJo@users.noreply.github.com>
2019-10-27 12:24:37 +1100

    Revert "Tarea clase 7"
    This reverts commit 78a6a04b32787c604b10d5f38e885db8ce21c86f.

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,15 +1,5 @@
-function probarValidarNombre() {
-  console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
-  );
-
-  console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
-  );
+function sumar(a, b) {
+  return a + b;
 }
 
-probarValidarNombre();
+console.assert(sumar(1, 2) === 3, 'la suma de 1 + 2 no dio 3');
