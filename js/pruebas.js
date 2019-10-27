78a6a04b32787c604b10d5f38e885db8ce21c86f
ClassicalJo <55909151+ClassicalJo@users.noreply.github.com>
2019-10-27 12:19:58 +1100

    Tarea clase 7
    Crea los archivos necesarios para cumplir con la tarea de la clase 7, y modifica tanto pruebas como index.html para acomodarse a los nuevos archivos.

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,5 +1,11 @@
-function sumar(a, b) {
-  return a + b;
-}
+console.assert(verificarInputMenosDe50Caracteres("1234567890-1234567890-1234567890-1234567890-1234567890") === false, 'la verificacion de la funcion con menos de 50 cantidad de caracteres no dio false');
 
-console.assert(sumar(1, 2) === 3, 'la suma de 1 + 2 no dio 3');
+console.assert(verificarInputMenosDe100Caracteres("1234567890-1234567890-1234567890-1234567890-12345678901234567890-1234567890-1234567890-1234567890-1234567890") === false, 'la verificaciones del input con menos de 100 caracteres no dio false!')
+
+console.assert(verificarInputContieneSoloLetrasYNumeros("aSDSA123!&*(@&*"), 'la verificacion de que el input contenga solo letras y numeros no dio false!')
+
+console.assert(verificarInputNoVacio("") === false, 'la verificacion de input vacio no dio false')
+
+console.assert(verificarInputContieneSoloLetras("!@#$!@!@#>><><asd314>"), "la verificacion de input conteniendo solo letras no dio false!")
+console.assert(verificarInputContieneSoloLetras("sd314"), "la verificacion de input conteniendo solo letras no dio false!")
+
