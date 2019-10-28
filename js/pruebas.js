86b77dc1951f515da7df7ba4be5ac5f9bae938aa
ClassicalJo <55909151+ClassicalJo@users.noreply.github.com>
2019-10-28 14:42:26 +1100

    Update pruebas.js

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,4 +1,5 @@
 console.assert(verificarInputMenosDe50Caracteres("1234567890-1234567890-1234567890-1234567890-1234567890") === false, 'la verificacion de la funcion con menos de 50 cantidad de caracteres no dio false');
+<<<<<<< HEAD
 
 console.assert(verificarInputMenosDe100Caracteres("1234567890-1234567890-1234567890-1234567890-12345678901234567890-1234567890-1234567890-1234567890-1234567890") === false, 'la verificaciones del input con menos de 100 caracteres no dio false!')
 
@@ -8,3 +9,15 @@ console.assert(verificarInputNoVacio("") === false, 'la verificacion de input va
 
 console.assert(verificarInputContieneSoloLetras("!@#$!@!@#>><><asd314>"), "la verificacion de input conteniendo solo letras no dio false!")
 console.assert(verificarInputContieneSoloLetras("sd314"), "la verificacion de input conteniendo solo letras no dio false!")
+=======
+
+console.assert(verificarInputMenosDe100Caracteres("1234567890-1234567890-1234567890-1234567890-12345678901234567890-1234567890-1234567890-1234567890-1234567890") === false, 'la verificaciones del input con menos de 100 caracteres no dio false!')
+
+console.assert(verificarInputContieneSoloLetrasYNumeros("aSDSA123!&*(@&*"), 'la verificacion de que el input contenga solo letras y numeros no dio false!')
+
+console.assert(verificarInputNoVacio("") === false, 'la verificacion de input vacio no dio false')
+
+console.assert(verificarInputContieneSoloLetras("!@#$!@!@#>><><asd314>"), "la verificacion de input conteniendo solo letras no dio false!")
+console.assert(verificarInputContieneSoloLetras("sd314"), "la verificacion de input conteniendo solo letras no dio false!")
+
+>>>>>>> parent of e5a7b0d... Revert "Tarea clase 7"
