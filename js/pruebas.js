0e54944bab4676d911e9618d9798da6dc4cb5326
ClassicalJo <55909151+ClassicalJo@users.noreply.github.com>
2019-10-28 15:02:01 +1100

    Update pruebas.js

--- js/pruebas.js
+++ js/pruebas.js
@@ -9,7 +9,6 @@ console.assert(verificarInputNoVacio("") === false, 'la verificacion de input va
 
 console.assert(verificarInputContieneSoloLetras("!@#$!@!@#>><><asd314>"), "la verificacion de input conteniendo solo letras no dio false!")
 console.assert(verificarInputContieneSoloLetras("sd314"), "la verificacion de input conteniendo solo letras no dio false!")
-=======
 
 console.assert(verificarInputMenosDe100Caracteres("1234567890-1234567890-1234567890-1234567890-12345678901234567890-1234567890-1234567890-1234567890-1234567890") === false, 'la verificaciones del input con menos de 100 caracteres no dio false!')
 
