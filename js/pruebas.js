286de1425e40ba3fda7c585178721c61a5c17735
ClassicalJo <55909151+ClassicalJo@users.noreply.github.com>
2019-10-28 17:05:01 +1100

    arregla pruebas.js

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,15 +1,19 @@
-function probarValidarNombre() {
-  console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
-  );
+console.assert(verificarInputMenosDe50Caracteres("1234567890-1234567890-1234567890-1234567890-1234567890") === false, 'la verificacion de la funcion con menos de 50 cantidad de caracteres no dio false');
 
-  console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
-  );
-}
+console.assert(verificarInputMenosDe100Caracteres("1234567890-1234567890-1234567890-1234567890-12345678901234567890-1234567890-1234567890-1234567890-1234567890") === false, 'la verificaciones del input con menos de 100 caracteres no dio false!')
 
-probarValidarNombre();
+console.assert(verificarInputContieneSoloLetrasYNumeros("aSDSA123!&*(@&*"), 'la verificacion de que el input contenga solo letras y numeros no dio false!')
+
+console.assert(verificarInputNoVacio("") === false, 'la verificacion de input vacio no dio false')
+
+console.assert(verificarInputContieneSoloLetras("!@#$!@!@#>><><asd314>"), "la verificacion de input conteniendo solo letras no dio false!")
+console.assert(verificarInputContieneSoloLetras("sd314"), "la verificacion de input conteniendo solo letras no dio false!")
+
+console.assert(verificarInputMenosDe100Caracteres("1234567890-1234567890-1234567890-1234567890-12345678901234567890-1234567890-1234567890-1234567890-1234567890") === false, 'la verificaciones del input con menos de 100 caracteres no dio false!')
+
+console.assert(verificarInputContieneSoloLetrasYNumeros("aSDSA123!&*(@&*"), 'la verificacion de que el input contenga solo letras y numeros no dio false!')
+
+console.assert(verificarInputNoVacio("") === false, 'la verificacion de input vacio no dio false')
+
+console.assert(verificarInputContieneSoloLetras("!@#$!@!@#>><><asd314>"), "la verificacion de input conteniendo solo letras no dio false!")
+console.assert(verificarInputContieneSoloLetras("sd314"), "la verificacion de input conteniendo solo letras no dio false!")
