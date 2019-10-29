4296db48ea2308d83989f806aba87b11b92693a5
fedeB1 <fedebracone@gmail.com>
2019-10-29 19:31:56 -0300

    pruebas para las validaciones

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,15 +1,42 @@
 function probarValidarNombre() {
   console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
+      validarNombre('') === 'Este campo no puede estar vacío',
       'Validar nombre no validó que el nombre no sea vacío',
   );
-
   console.assert(
       validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
+          'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa') ===
       'Este campo debe tener menos de 50 caracteres',
       'Validar nombre no validó que el nombre sea menor a 50 caracteres',
   );
+  console.assert(
+    validarNombre(
+        '1') ===
+    'Este campo debe contener solo letras',
+    'Validar nombre no validó que el nombre sean solo letras',
+    );
 }
 
+function probarValidarCiudad() {
+    console.assert(
+        validarCiudad('') === 'Este campo no puede estar vacío',
+        'Validar ciudad no validó que la Ciudad no sea vacío',
+    );
+}
+
+function probarDescripcionRegalo() {
+    console.assert(
+        validarDescripcionRegalo('') === 'Este campo no puede estar vacío',
+        'Validar descripción no validó que la descripción no sea vacío',
+    );
+    console.assert(
+        validarDescripcionRegalo(
+            'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa') ===
+        'Este campo debe tener menos de 250 caracteres',
+        'Validar descripción regalo no validó que la descripción sea menor a 250 caracteres',
+    );
+  }
+
 probarValidarNombre();
+probarValidarCiudad();
+probarDescripcionRegalo();
