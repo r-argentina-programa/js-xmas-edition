ddc9d586c8f048e86cb52315932d631e4f7a6d2d
Ja-boop <agusmisiones@gmail.com>
2019-11-22 16:06:26 -0300

    Updatte

---
+++ js/main-viktor.js
@@ -0,0 +1,23 @@
+const $form = document.querySelector('#carta-a-santa');
+
+const nombre = $form.nombre.value;
+const ciudad = $form.ciudad.value;
+const comportamiento = $form.comportamiento.value;
+const descripcionRegalo = $form['descripcion-regalo'].value;
+
+console.log(nombre);
+console.log(ciudad);
+console.log(comportamiento);
+console.log(descripcionRegalo);
+
+function validarNombre(nombre) {
+    if (nombre.length === 0) {
+        return ('Este campo debe tener al menos 1 caracter')
+    }
+
+    if (nombre.length >= 50) {
+        return ('Este campo debe tener menos de 50 caracteres')
+    }
+
+    return '';
+}
--- js/pruebas.js
+++ js/pruebas.js
@@ -1,4 +1,4 @@
-function probarValidarNombre() {
+/*function probarValidarNombre() {
   console.assert(
       validarNombre('') === 'Este campo debe tener al menos 1 caracter',
       'Validar nombre no validó que el nombre no sea vacío',
@@ -13,3 +13,4 @@ function probarValidarNombre() {
 }
 
 probarValidarNombre();
+*/
