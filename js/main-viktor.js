dba06363950f837aaa63bf441248257bc2bad0ce
Ja-boop <agusmisiones@gmail.com>
2019-12-05 21:12:03 -0300

    Update main-viktor.js

--- js/main-viktor.js
+++ js/main-viktor.js
@@ -67,7 +67,12 @@ function validarFormulario(event) {
         'descripcion-regalo': errorDescripcionRegalo,
     };
 
-    manejarErrores(errores);
+   const esExito = manejarErrores(errores) === 0;
+   
+   if(esExito){
+       $form.className = 'oculto';
+       document.querySelector('#exito').className = '';
+   }
 
     event.preventDefault();
 }
@@ -75,46 +80,24 @@ function validarFormulario(event) {
 function manejarErrores(errores) {
 
     const keys = Object.keys(errores);
-    console.log(keys);
+    const $errores = document.querySelector('#errores')
+    let cantidadErrores = 0;
 
     keys.forEach(function (key) {
         const error = errores[key];
 
         if (error) {    
             $form[key].className = "error"
+            cantidadErrores++;
+
+            const $error = document.createElement('li');
+            $error.innerText = error; //<li>gsdagsagasdgasg</li>
+            $errores.appendChild($error);
+
         } else {
             $form[key].className = ""
         }
     });
-
-
-
-    // errorNombre = errores.nombre; //nombre
-    // errorCiudad = errores.ciudad; //ciudad
-    // errorDescripcionRegalo = errores.descripcionRegalo //descripcionRegalo
-
-    // if(errorNombre) {
-    //     $form.nombre.className = "error";
-    // } else {
-    //     $form.nombre.className = "";
-    // }
-
-    // if(errorCiudad) {
-    //     $form.ciudad.className = "error";
-    // } else {
-    //     $form.ciudad.className = "";
-    // }
-
-    // if(errores.descripcionRegalo) {
-    //     $form['descripcion-regalo'].className = "error";
-    // } else {
-    //     $form['descripcion-regalo'].className = "";
-    // }
-
-
-
-
-
 
 }
 
