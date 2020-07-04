e3990cf0ef79c058bcc8e0631310d89b3450424b
Marcos8i22o <marcos.jinno@gmail.com>
2020-07-04 13:03:04 -0300

    Agrega prueba validacion ingreso integrantes

--- clase-6/tarea 1/pruebas.js
+++ clase-6/tarea 1/pruebas.js
@@ -1,8 +1,8 @@
 function probarValidarIngresoIntegrantes() {
 
     console.assert(
-        validarIngresoIntegrantes (String(-1) ===  
-            "El número ingresado es incorrecto. No puede ser menor a cero."),
+        validarIngresoIntegrantes (0) === "El número ingresado es incorrecto. No puede ser menor a cero.", 
+            "ValidarIngresoIntegrantes no validó que el nuúmero ingresado sea correcto.",
     );
 
 }
