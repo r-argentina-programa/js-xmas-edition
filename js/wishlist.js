7db1d5f77283629c90f61efb5b2b5d3b587b375e
Uciel <uciel.lud@gmail.com>
2021-02-01 11:31:29 -0300

    Se termina el nivel2

--- js/wishlist.js
+++ js/wishlist.js
@@ -2,21 +2,13 @@ function displayWishes() {
 
     const $wishList = document.querySelector('.wish-list')
 
-    const $wishOfAUser = document.createElement('li')
-
     for (i = 0; i < localStorage.length; i++) {
-        let userKey = 'user' + i
 
+        const $wishOfAUser = document.createElement('li')
+        let userKey = 'user' + i
         let userData = JSON.parse(localStorage.getItem(userKey))
 
-
         $wishOfAUser.appendChild(document.createTextNode(userData.giftDescription))
-
         $wishList.appendChild($wishOfAUser)
-
     }
-
-
 }
-
-displayWishes()
